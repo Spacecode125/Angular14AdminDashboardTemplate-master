b4d6c07961b6fd4bc54a1745273bbf89c7c54430
helmi <helmi.br1999@gmail.com>
2023-05-19 21:38:25 +0100

    add some changes in profile

--- src/app/pages/users-profile/users-profile.component.ts
+++ src/app/pages/users-profile/users-profile.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-users-profile',
   templateUrl: './users-profile.component.html',
-  styleUrls: ['./users-profile.component.css']
+  styleUrls: ['./users-profile.component.css'],
 })
 export class UsersProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  user: any;
+  selectedImage: any;
+  constructor(private sanitizer: DomSanitizer) {}
+  handleFileInput(event: any) {
+    this.selectedImage = event.target.files[0];
   }
 
+  getImageSource(): SafeUrl {
+    if (this.selectedImage) {
+      return this.sanitizer.bypassSecurityTrustUrl(
+        URL.createObjectURL(this.selectedImage)
+      );
+    } else if (this.user && this.user.image) {
+      const imageUrl = 'http://localhost:3000/api/' + this.user.image;
+      return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+    } else {
+      return '';
+    }
+  }
+
+  deleteImage(): void {
+    this.selectedImage = null;
+  }
+  ngOnInit(): void {
+    const output = window.localStorage.getItem('user');
+    this.user = output ? JSON.parse(output) : null;
+  }
 }
